519a9b2c5db04767d66eb268c538c4d1398f7e1f
Michael Mottola <mottola64@gmail.com>
2019-06-06 00:53:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,13 @@ class Dog
 
 attr_accessor :name, :breed, :id
 
-  def initialize(:id=nil, :name, :breed)
+  def initialize(id=nil, name, breed)
     @id = :id
     @name = :name
     @breed = :breed
   end
 
-  def attributes(id=nil, :name, :breed)
+  def attributes(id=nil, name, breed)
     @id = id
     @name = :name
     @breed = :breed
